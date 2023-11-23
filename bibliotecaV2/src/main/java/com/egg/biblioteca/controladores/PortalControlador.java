c36946b65bb4c4b0caa472ddaa52a6504a82c2fc
didacuscha <charrydiego16@gmail.com>
2023-11-23 18:49:08 -0500

    added thymeleaf security features to html, missing autorlist and editoriallist

--- bibliotecaV2/src/main/java/com/egg/biblioteca/controladores/PortalControlador.java
+++ bibliotecaV2/src/main/java/com/egg/biblioteca/controladores/PortalControlador.java
@@ -1,7 +1,9 @@
 package com.egg.biblioteca.controladores;
 
+import com.egg.biblioteca.entidades.Usuario;
 import com.egg.biblioteca.excepciones.MiException;
 import com.egg.biblioteca.servicios.UsuarioServicio;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -55,8 +57,12 @@ public class PortalControlador {
 
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping("/inicio")
-    public String inicio() {
+    public String inicio(HttpSession session) {
+        Usuario logueado =  (Usuario) session.getAttribute("usuariosession");
 
+        if (logueado.getRol().toString().equals("ADMIN")) {
+            return "redirect:/admin/dashboard";
+        }
         return "inicio.html";
     }
 }
